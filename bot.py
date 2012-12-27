a40181519d95203dd337b23541bfc47669dd593f
walle1357 <foxwilson123@gmail.com>
2012-12-27 15:01:10 -0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -13,6 +13,23 @@ from includes import *
 #    count = int(round(((data.count("revert")+data.count("rvv")+data.count("rv vand")+data.count("rv ")-data.count("rv good faith")-data.count("reverted good faith")-data.count("reverting good faith")-data.count("help:reverting"))/5.0)+1)) #Find the amount of hits of the word "revert" then subtract good faith and duplicate occurances, divide by 5 to average, and add 1 so it rounds up.
 #    totalEdits = int(round(data.count('<rc type')/5.0)) #Calculate total edits in same manner (temp fix)
 #    return (totalEdits, count) #Return a tuple of the results
+usernameKey = 'lgname' 
+passwordKey = 'lgpassword' #Define POST variable names
+loginUrl = "http://en.wikipedia.org/w/api.php?format=xml&action=login" #Define request URL
+loginInformation = {usernameKey: uname, passwordKey: password} # Creates a request dictionary
+loginInformatione = urllib.urlencode(loginInformation) # Encode the login information.
+loginRequest = url.Request(loginUrl, loginInformatione) # Creates a Request that is used to login
+loginResponse = ClientCookie.urlopen(loginRequest) # Sends the login to Wikipedia and stores the PHP session ID.
+loginResponse = loginResponse.read() # Get the HTML/XML from Wikipedia.
+loginToken = loginResponse.split('token="')[1].split('" cookie')[0] #Get 1st login token
+loginInformation["lgtoken"] = loginToken #Add token to dictionary
+loginInformatione = urllib.urlencode(loginInformation) #Re-encode with new login token
+loginRequest = url.Request(loginUrl, loginInformatione) #Login again (required by API), passing the login token this time
+loginResponse = ClientCookie.urlopen(loginRequest) #Get the cookie from the login request
+loginResponse = loginResponse.read() #Set the response to the .read of the response (parsable)
+print loginResponse #Print login response for debugging
+x = ClientCookie.urlopen("http://en.wikipedia.org/w/api.php?action=tokens&format=xml").read().split('edittoken="')[1].split('" />')[0] #Get edit token - required to edit the template
+print x #Print the token for debugging
 theTuple = getCounts() #Define tuple as array
 open("/home/pibot/webdocs/report.txt", "a").writelines(getSummaries(True))
 print "Edit Per Minute: ", theTuple[0] #Output Edits per Minute to Output Screen for Debugging
@@ -36,23 +53,7 @@ if str(theTuple[0]) + " " + str(theTuple[1]) not in b:
 {{documentation}}
 </noinclude>""" #Define template, adding in values
 	print edit #Print for Debugging
-	usernameKey = 'lgname' 
-	passwordKey = 'lgpassword' #Define POST variable names
-	loginUrl = "http://en.wikipedia.org/w/api.php?format=xml&action=login" #Define request URL
-	loginInformation = {usernameKey: uname, passwordKey: password} # Creates a request dictionary
-	loginInformatione = urllib.urlencode(loginInformation) # Encode the login information.
-	loginRequest = url.Request(loginUrl, loginInformatione) # Creates a Request that is used to login
-	loginResponse = ClientCookie.urlopen(loginRequest) # Sends the login to Wikipedia and stores the PHP session ID.
-	loginResponse = loginResponse.read() # Get the HTML/XML from Wikipedia.
-	loginToken = loginResponse.split('token="')[1].split('" cookie')[0] #Get 1st login token
-	loginInformation["lgtoken"] = loginToken #Add token to dictionary
-	loginInformatione = urllib.urlencode(loginInformation) #Re-encode with new login token
-	loginRequest = url.Request(loginUrl, loginInformatione) #Login again (required by API), passing the login token this time
-	loginResponse = ClientCookie.urlopen(loginRequest) #Get the cookie from the login request
-	loginResponse = loginResponse.read() #Set the response to the .read of the response (parsable)
-	print loginResponse #Print login response for debugging
-	x = ClientCookie.urlopen("http://en.wikipedia.org/w/api.php?action=tokens&format=xml").read().split('edittoken="')[1].split('" />')[0] #Get edit token - required to edit the template
-	print x #Print the token for debugging
+
 	editInfo = url.Request("http://en.wikipedia.org/w/api.php", urllib.urlencode({"format": "xml", "action": "edit", "token": x, "summary": "Updating Vandalism Information Template (Unapproved bot editing [[Wikipedia:Bot_policy#Requests_for_approval|in own userspace]])", "text": edit, "title": "User:VoxelBot/Vandalism information"})) #Define the edit query and execute it
 	response = ClientCookie.urlopen(editInfo).read() #Get the response
 	print response #Print for debugging
