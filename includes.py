64b0b48d80bb01d4547d25bd4b54e35f6d5ff3a0
Samuel Damashek <samuel.damashek@gmail.com>
2012-12-27 15:04:45 -0500

    Added incorrect

--- includes.py
+++ includes.py
@@ -6,7 +6,7 @@ from xml.dom import minidom
 def classifyEdit(summary):
 	summary = summary.lower()
 	vandalism = ["revert", "rv "]
-	notVandalism = ["good faith", "agf", "unsourced", "unreferenced", "self", "speculat", "original research", "rv tag", "typo"]
+	notVandalism = ["good faith", "agf", "unsourced", "unreferenced", "self", "speculat", "original research", "rv tag", "typo", "incorrect"]
 	for i in notVandalism:
 		if i in summary:
 			return False
