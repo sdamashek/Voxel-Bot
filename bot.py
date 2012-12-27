db8a0991a59ed8c68c02a85eb28e5ab2be2d56a6
walle1357 <foxwilson123@gmail.com>
2012-12-27 12:06:15 -0500

    use includes.py

--- bot.py
+++ bot.py
@@ -4,6 +4,7 @@ import urllib
 import ClientCookie
 import re
 from logininfo import * #import various libraries and the login info
+from includes import *
 def getCountThingy():
     currentTime = time.strftime("%Y-%m-%dT%H:%M:%SZ", (time.gmtime(time.time()-300))) #Format time according to MediaWiki API Specifications
     x = "http://en.wikipedia.org/w/api.php?action=query&list=recentchanges&rcstart="+currentTime+"&rcend="+time.strftime("&Y-&m-&dT%H:%M:%SZ", time.gmtime())+"&rclimit=500&rcdir=newer&rcprop=comment&format=xml" #Define RecentChanges Query
@@ -12,7 +13,7 @@ def getCountThingy():
     count = int(round(((data.count("revert")+data.count("rvv")+data.count("rv vand")+data.count("rv ")-data.count("rv good faith")-data.count("reverted good faith")-data.count("reverting good faith")-data.count("help:reverting"))/5.0)+1)) #Find the amount of hits of the word "revert" then subtract good faith and duplicate occurances, divide by 5 to average, and add 1 so it rounds up.
     totalEdits = int(round(data.count('<rc type')/5.0)) #Calculate total edits in same manner (temp fix)
     return (totalEdits, count) #Return a tuple of the results
-theTuple = getCountThingy() #Define tuple as array
+theTuple = getCounts #Define tuple as array
 print "Edit Per Minute: ", theTuple[0] #Output Edits per Minute to Output Screen for Debugging
 print "RV Per Minute: ", theTuple[1] #Same, but for reverts
 a = open("bot.txt") #open connection to the previous value file, bot.txt
