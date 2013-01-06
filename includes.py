6e00f64ab3840240756641c3fdc94dea8bb14cb9
Fox C. Wilson <foxwilson123@gmail.com>
2013-01-05 20:39:40 -0500

    Revert "Merge branches 'master' and 'devel' of https://github.com/Vacation9/Voxel-Bot"
    This reverts commit 39a7846af34d9aed26fd9415e72a22e70e38b2d3, reversing
    changes made to 187995ac87fceee7a19a5960adf5c97ab9b82376.
    
    YAY!!!`

--- includes.py
+++ includes.py
@@ -1,4 +1,3 @@
-<<<<<<< .merge_file_bOm3AL
 import ClientCookie as CC
 import time
 import urllib2 as url
@@ -40,35 +39,3 @@ def getSummaries(thatAreVandalism):
 		if classifyEdit(i) == thatAreVandalism:
 			e.append(i+"\n\n")
 	return e
-=======
-from ClientCookie import *
-import time
-import urllib2 as url
-import urllib
-from xml.dom import minidom
-def classifyEdit(summary):
-	summary = summary.lower()
-	vandalism = ["revert", "rv"]
-	notVandalism = ["good faith", "agf"]
-	for i in notVandalism:
-		if i in summary:
-			return False
-	for i in vandalism:
-		if i in summary:
-			return True
-	return False
-def getSummaryList():
-	currentTime = time.strftime("%Y-%m-%dT%H:%M:%SZ", (time.gmtime(time.time()-300))) #Format time according to MediaWiki API Specifications
-	x = "http://en.wikipedia.org/w/api.php?action=query&list=recentchanges&rcstart="+currentTime+"&rcend="+time.strftime("&Y-&m-&dT%H:%M:%SZ", time.gmtime())+"&rclimit=500&rcdir=newer&rcprop=comment&format=xml" #Define RecentChanges Query
-	data = url.urlopen(x).read() #Make the request
-	data = data.lower() #Make everything lowercase for ease of parsing
-	dom = minidom.parseString(data)
-	changes = dom.getElementsByTagName("rc")
-	return [i.attributes['comment'].value for i in changes]
-def getCounts():
-	sums = getSummaryList()
-	revert = int(round((len(([i for i in sums if classifyEdit(i)]))+1)/5.0))
-	total = int(round((len(sums)+1)/5.0))
-	return (total, revert)
-
->>>>>>> .merge_file_KF3cYI
