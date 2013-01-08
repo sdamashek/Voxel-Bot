ab5a0aabd047c9c4faa5b95333652ce9fa4470fd
Samuel Damashek <samuel.damashek@gmail.com>
2013-01-07 21:40:45 -0500

    (Hopefully) Implement query-continue/30 minute
    Edit every thirty minutes (cron job still needs to be changed) and use
    query continue, hopefully correctly.

--- includes.py
+++ includes.py
@@ -18,14 +18,22 @@ def classifyEdit(summary):
 				return False
 			return True
 	return False
-def getSummaryList():
-	currentTime = time.strftime("%Y-%m-%dT%H:%M:%SZ", (time.gmtime(time.time()-1200))) #Format time according to MediaWiki API Specifications
-	x = "http://en.wikipedia.org/w/api.php?action=query&list=recentchanges&rcstart="+currentTime+"&rcend="+time.strftime("&Y-&m-&dT%H:%M:%SZ", time.gmtime())+"&rclimit=5000&rcdir=newer&rcprop=comment|ids&format=xml" #Define RecentChanges Query
-	data = CC.urlopen(x).read() #Make the request
+def recentChangesGet(timeFrom): #Because everybody likes recursiveness
+	print timeFrom
+	data = CC.urlopen("http://en.wikipedia.org/w/api.php?action=query&list=recentchanges&rcstart="+timeFrom+"&rcend="+time.strftime("%Y-%m-%dT%H:%M:%SZ", time.gmtime())+"&rclimit=5000&rcdir=newer&rcprop=comment|ids&format=xml").read() #Make the request
 	data = data.lower() #Make everything lowercase for ease of parsing
 	dom = minidom.parseString(data)
+	recentChanges = dom.getElementsByTagName("recentchanges")
 	changes = dom.getElementsByTagName("rc")
-	return [(i.attributes['comment'].value+" Rev Link: http://en.wikipedia.org/w/index.php?diff="+i.attributes['revid'].value) for i in changes]
+	try:
+		return [(recentChangesGet(recentChanges[0].attributes['rcstart'].value))+((i.attributes['comment'].value+" Rev Link: http://en.wikipedia.org/w/index.php?diff="+i.attributes['revid'].value) for i in changes)]
+	except KeyError:
+		return [(i.attributes['comment'].value+" Rev Link: http://en.wikipedia.org/w/index.php?diff="+i.attributes['revid'].value) for i in changes]
+def getSummaryList():
+	currentTime = time.strftime("%Y-%m-%dT%H:%M:%SZ", (time.gmtime(time.time()-1800))) #Format time according to MediaWiki API Specifications
+	rc=recentChangesGet(currentTime)
+	print rc
+	return rc
 def getCounts():
 	sums = getSummaryList()
 	rv = len(([i for i in sums if classifyEdit(i)]))
