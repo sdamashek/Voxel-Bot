92f7796b80046af1c7784ffd70b13b947c579b89
Samuel Damashek <samuel.damashek@gmail.com>
2013-01-10 11:03:57 -0500

    Revert "End Trial"
    This reverts commit d8acfc06ef5f0f7949a7e36287dd279467cfd66b.

--- bot.py
+++ bot.py
@@ -39,8 +39,8 @@ b = a.read() #set value of a to b
 templatecontent = "http://en.wikipedia.org/w/api.php?action=query&titles=Template:Vandalism_information&format=xml&prop=revisions&rvprop=content" #Get Template contents to do manual override check
 templatecontent = ClientCookie.urlopen(templatecontent).read() #Make the request
 print templatecontent
-#if str(theTuple[1]) not in b and "&lt;!--NOOVERRIDE--&gt;" in templatecontent:
-if False:
+if str(theTuple[1]) not in b and "&lt;!--NOOVERRIDE--&gt;" in templatecontent:
+#if False:
 	a.close() #close to make way for write connection
 	a = open("bot.txt", "w") #Open in write connection
 	a.write(str(theTuple[1])) #Add current values to previous value file
